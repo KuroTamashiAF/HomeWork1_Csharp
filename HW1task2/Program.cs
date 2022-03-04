bab6e5185de8aabf0c9b0a7b4b2ec0f8fb097b5e
Alex <chugun777akk@mail.ru>
2022-03-04 17:59:11 +0700

    task2 is done

---
+++ HW1task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter the first number: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the third number: ");
+int num3 = int.Parse(Console.ReadLine());
+int max = num1;
+if (num2 > max)
+    max=num2;
+if (num3 > max)
+    max=num3;
+Console.WriteLine("The largest number you entered: ");
+Console.WriteLine(max);
