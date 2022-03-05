4c4c56374a0506b67a66615c88a7192366b158f4
Alex <chugun777akk@mail.ru>
2022-03-05 13:49:37 +0700

    task 4 is done

---
+++ HW1task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter a three-digit number: ");
+int num = int.Parse(Console.ReadLine());
+if (num > 99 && num < 999)
+{
+    int last_number = num % 10;
+    Console.WriteLine("The last digit of this three-digit number is  ");
+    Console.WriteLine(last_number);
+}
+else
+    Console.WriteLine("You entered a non-three-digit number");
