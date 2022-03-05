f7631805f123b81d52e48cc498a72d756aa2315d
Alex <chugun777akk@mail.ru>
2022-03-05 13:59:31 +0700

    task5 is done

---
+++ HW1task5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter a three-digit number: ");
+int num = int.Parse(Console.ReadLine());
+if (num > 99 && num < 999)
+{
+    int last_number = num / 10 % 10;
+    Console.WriteLine("The pre-last digit of a given three-digit number is  ");
+    Console.WriteLine(last_number);
+}
+else
+    Console.WriteLine("You entered a non-three-digit number");
