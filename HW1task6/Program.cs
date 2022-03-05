a1361b9faba4a05204506b6a70cd5781846592e6
Alex <chugun777akk@mail.ru>
2022-03-05 17:23:19 +0700

    task6 is done

--- HW1task6/Program.cs
+++ HW1task6/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int num = new Random().Next(10, 100);
+Console.WriteLine(num);
+int first_digit = num / 10;
+int second_digit = num % 10;
+if (first_digit > second_digit)
+{
+    Console.Write("Largest digit: ");
+    Console.Write(first_digit);
+}
+else
+{
+    Console.Write("Largest digit: ");
+    Console.Write(second_digit);
+}
